f671ff5ad24f447a74519ff528411784ef23c5c9
hugmeir <fraserbn@gmail.com>
2023-05-14 15:00:44 +0200

    BLU: Dying to Final Sting/Self-Destruct won't be counted as a death
    Final Sting is essentially a pocket LB2; most BLU fights end with
    most of the team exploding themselves for a huge burst of damage.

--- src/parser/jobs/blu/changelog.tsx
+++ src/parser/jobs/blu/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2023-05-14'),
+		Changes: () => <>Dying to Final Sting and Self-Destruct won't be counted as a death</>,
+		contributors: [CONTRIBUTORS.HUGMEIR],
+	},
+	{
 		date: new Date('2023-05-08'),
 		Changes: () => <>Support for BLU for 6.0 - 6.4</>,
 		contributors: [CONTRIBUTORS.HUGMEIR],
