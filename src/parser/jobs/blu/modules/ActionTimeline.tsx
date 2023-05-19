7adedc34f3a48cf70d4f43f759a3db09490fbc22
hugmeir <fraserbn@gmail.com>
2023-05-19 14:13:51 +0200

    BLU: Track a bunch of useful actions in the timeline
    Note that this is somewhat buggy at the moment due to abilities
    with shared cooldowns, like Matra Magic / Dragon Force, or
    Eruption / Feather Rain.
    
    Currently the timeline code just picks whatever was listed first
    in the code; I've spent a while trying to make it display whatever
    was first used, but we don't have that data when setting up
    the timeline rows, and delaying setting those up breaks things
    in ways I don't quite understand.
    
    Regardless, this is groundwork to build on.

---
+++ src/parser/jobs/blu/modules/ActionTimeline.tsx
@@ -0,0 +1,44 @@
+import {ActionRow, ActionTimeline as CoreActionTimeline} from 'parser/core/modules/ActionTimeline'
+
+// TODO: Would be really nice if ActionTimeline picked the name of the first
+// one used, instead of the first one in the array.
+export class ActionTimeline extends CoreActionTimeline {
+	static override rows: ActionRow[] = [
+		...CoreActionTimeline.rows,
+		'NIGHTBLOOM',
+		'TRIPLE_TRIDENT',
+		'THE_ROSE_OF_DESTRUCTION',
+		'SHOCK_STRIKE',
+		'BLU_MOUNTAIN_BUSTER',
+		'GLASS_DANCE',
+		'SURPANAKHA',
+		['MATRA_MAGIC', 'DRAGON_FORCE', 'ANGELS_SNACK'],
+		'FEATHER_RAIN',
+		'ERUPTION',
+		['PHANTOM_FLURRY', 'PHANTOM_FLURRY_KICK'],
+		['QUASAR', 'J_KICK'],
+		{
+			label: 'Raid Buffs',
+			content: [
+				'PECULIAR_LIGHT',
+				'OFF_GUARD',
+			],
+		},
+		'COLD_FOG',
+
+		// Standard role actions
+		'LUCID_DREAMING',
+		'SWIFTCAST',
+
+		// Mit
+		'ADDLE',
+		'MAGIC_HAMMER',
+
+		// Ressurect
+		'ANGEL_WHISPER',
+
+		// Tanking CDs
+		'DEVOUR',
+		'CHELONIAN_GATE',
+	]
+}
